c29643552cba89859f9db4374cc2e25c16a479d8
SHASHANK-HM-51 <shashank.hm@gaiansolutions.com>
2023-12-15 19:38:50 +0530

    issue fix

--- APIs/comments.js
+++ APIs/comments.js
@@ -12,10 +12,11 @@ const auth = {
 //https://gaiansolutions.atlassian.net/rest/api/2/search
 
 //Gets all issues in a particular project using the Jira Cloud REST API
-async function getDayComments() {
+async function getDayComments(startAt = 0) {
   try {
     const baseUrl = "https://" + domain + ".atlassian.net";
 
+    console.log('running for', startAt);
     const config = {
       method: "post",
       url: baseUrl + `/rest/api/2/search`,
@@ -23,12 +24,20 @@ async function getDayComments() {
       auth: auth,
       data: {
         jql: "updatedDate >= startOfDay() AND updatedDate <= endOfDay() ORDER BY updated DESC",
-        fields: ["key", "project", "assignee", "status", "comment", "issuetype", "updated"]
+        fields: ["key", "project", "assignee", "status", "comment", "issuetype", "updated"],
+        maxResults: 10000,
+        startAt
       }
     };
     const response = await axios.request(config);
-    console.log('got response');
-    return response.data;
+    const result = response.data;
+    const issues = response.data.issues;
+    console.log('got response: ', Object.keys(response.data), result.maxResults, result.startAt, result.total, issues.length);
+    if(result.startAt + issues.length < result.total) {
+      const newIssues = await getDayComments(startAt + result.maxResults);
+      return issues.concat(newIssues);
+    }
+    return issues;
   } catch (error) {
     console.error('error: ', error);
     // console.log("error: ");
